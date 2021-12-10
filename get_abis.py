db53d77eaecdd9b954a95b5cbdc38601f2c8ed54
mzmyslowski <schizoburger@gmail.com>
2021-12-10 13:38:12 +0100

    Improved get_abis.py script

--- get_abis.py
+++ get_abis.py
@@ -11,8 +11,14 @@ ETHERSCAN_ABI_URL = 'https://api.etherscan.io/api?module=contract&action=getabi&
 
 
 def main():
-    already_downloaded = [abi[:-5] for abi in os.listdir('ABIs') if '.json' in abi]
-    with open('nfts.json', 'r') as f:
+    addresses_file = input('Type name of the file with addresses: ')
+    while not os.path.exists(addresses_file):
+        addresses_file = input(f'{addresses_file} doesn\'t exists. Type name of the file with addresses: ')
+    path_to_save = input('Type path to save addresses (e.g. ABIs/tokens/): ')
+    if not os.path.exists(path_to_save):
+        os.makedirs(path_to_save)
+    already_downloaded = [abi[:-5] for abi in os.listdir(path_to_save) if '.json' in abi]
+    with open(addresses_file, 'r') as f:
         nfts = json.load(f)
     addresses = nfts.values()
     for id, address in enumerate(addresses):
@@ -20,7 +26,8 @@ def main():
         if address not in already_downloaded:
             print('Getting abi for', address)
             a = json.loads(requests.get(ETHERSCAN_ABI_URL.format(address, ETHERSCAN_API_KEY)).text)
-            with open(f'ABIs/{address}.json', 'w') as f:
+            path_to_file = os.path.join(path_to_save, f'{address}.json')
+            with open(path_to_file, 'w') as f:
                 json.dump(a['result'], f)
         else:
             print('ABI for', address, 'already exists')
