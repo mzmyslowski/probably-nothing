70b3d839a9ea26dd8b70f13bbb91c817d5cf5f18
mzmyslowski <schizoburger@gmail.com>
2021-12-10 13:36:56 +0100

    Extended parse_tokens_nft.py to scrape tokens

--- parse_tokens_nft.py
+++ parse_tokens_nft.py
@@ -4,22 +4,31 @@ import os
 import requests
 import time
 
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+
 
 def main():
-    while 'nfts.json' in os.listdir():
-        input('nfts.json file already exists! Delete it and hit enter.')
-    nft_dict = {}
-    for i in range(1, 250):
-        print(i)
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        a = requests.get(f'https://etherscan.io/tokens-nft?ps=100&p={i}', headers=headers).text
-        soup = BeautifulSoup(a)
+    file_to_save = input('Type name of the json file to save addresses: ')
+    while file_to_save in os.listdir():
+        file_to_save = input(f'{file_to_save} already exists. Type name of the json file to save addresses: ')
+    to_download = input('Type T for Tokens or N for NFTs: ')
+    while to_download not in ['N', 'T']:
+        to_download = input('Don\'t joke around. Type T for Tokens or N for NFTs: ')
+    pages = int(input('Type number of pages to download: '))
+    addresses_dict = {}
+    for i in range(1, pages):
+        print(f'{i}/{pages}')
+        a = requests.get(f'https://etherscan.io/tokens?ps=100&p={i}', headers=HEADERS).text
+        soup = BeautifulSoup(a, 'html.parser')
         nft_list = soup.find_all('a', attrs={'class': 'text-primary'})
         for nft in nft_list:
-            nft_dict[nft.string] = nft.get('title')
-        time.sleep(0.1)
-        with open('nfts.json', 'w') as f:
-            json.dump(nft_dict, f)
+            if to_download == 'T':
+                addresses_dict[nft.string] = nft.get('href').replace('/token/', '')
+            else:
+                addresses_dict[nft.string] = nft.get('title')
+        time.sleep(0.05)
+        with open(file_to_save, 'w') as f:
+            json.dump(addresses_dict, f)
 
 
 if __name__ == "__main__":
